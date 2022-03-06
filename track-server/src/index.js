680caf413e35d73a765b6b4bfe02ac01a29b7991
Barathraj N <barathraj6@gmail.com>
2022-03-06 13:37:15 +0530

    Added Signup Auth

---
+++ track-server/src/index.js
@@ -0,0 +1,30 @@
+require('./models/User');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes')
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = 'mongodb+srv://admin:vBa2VBQ3mS6t0O6d@track-cluster.00uae.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(mongoUri);
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+
+mongoose.connection.on('error',(err) => {
+    console.log('Error connecting to mongo instance', err);
+});
+
+app.get('/', (req,res) => {
+    res.send('Hello there');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+});
